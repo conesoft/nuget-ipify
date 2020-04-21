8fdf43440a9b55eede7757e1d1b413e124dca92c
David Spörri <davepermen@hotmail.com>
2020-04-21 20:02:21 +0200

    first publish

---
+++ src/Conesoft.Ipify/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Conesoft.Ipify
+{
+    class Client
+    {
+        readonly HttpClient client;
+
+        public Client(HttpClient client)
+        {
+            this.client = client;
+            client.BaseAddress = new Uri(@"https://api.ipify.org/");
+        }
+
+        public async Task<IPAddress> GetPublicIPAddress() => IPAddress.Parse(await (await client.GetAsync("")).Content.ReadAsStringAsync());
+    }
+}
